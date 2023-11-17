f36e1f482d4961ffcbf5159ab38a40702be3fafd
byronpendason <byronarnold@gmail.com>
2023-11-16 21:20:25 -0500

    update runetyper-webhelper.js

--- runetyper-webhelper.js
+++ runetyper-webhelper.js
@@ -12,10 +12,10 @@ var APP_PREFIX = 'rt_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_01';
+var VERSION = 'version_02';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
-  `${GHPATH}/`
+  `${GHPATH}`
 ]
