39087362fcdd553f9ddfbe01f4a014bed8004c21
byronpendason <byronpendason@gmail.com>
2025-08-10 17:01:22 -0400

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -208,7 +208,7 @@ class Calendar {
 	}
 	getNewMoon(date) {
 		date = new Date(AstroCalc.nextNewMoon(date));
-		while (AstroCalc.getMoonIllumination(date) < 0.015) {
+		while (AstroCalc.getMoonIllumination(date) < 0.0175) {
 			date.setDate(date.getDate() + 1);
 		}
 		date.setUTCHours(0, 0, 0, 0);
